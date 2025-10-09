ca47bbe08a3b8c336e4282d4377cf1329a5ef278
Walkorion <stefantosev15@gmail.com>
2025-10-09 15:11:06 +0200

    Add blur and change navbar

--- duva-li/src/components/Navbar.tsx
+++ duva-li/src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import DuvaLiLogo from "../assets/DuvaLiLogo.png";
+import DuvaLiLogo from "../assets/duvali-removebg.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { FaSearch } from 'react-icons/fa'; 
 
 interface NavbarProps {
   onSearch: (city: string) => void;
@@ -26,44 +27,41 @@ export default function Navbar({ onSearch }: NavbarProps) {
       <div className="nav-logo">
         <img src={DuvaLiLogo} alt="Weather Logo" />
       </div>
+
+     <div className="search-container"> 
+        <input 
+          type="text" 
+          placeholder="search anything" 
+          value={city} 
+          onChange={(e) => setCity(e.target.value)} 
+          onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+          className="search-input" 
+        /> 
+        <button onClick={handleSearch} className="search-button-submit"> 
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="20" 
+            height="20" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+          > 
+            <circle cx="11" cy="11" r="8"></circle> 
+            <path d="m21 21-4.35-4.35"></path> 
+          </svg> 
+        </button> 
+      </div> 
+
       <div className="nav-links">
          <div className="nav-links">
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/heatmap" className="nav-link">Heat Map</Link>
           </div>
       </div>
-      <div className={`search-container ${isSearchExpanded ? "expanded" : ""}`}>
-        <button
-          className="search-icon-button"
-          onClick={toggleSearch}
-          aria-label="Toggle search"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.35-4.35"></path>
-          </svg>
-        </button>
-        <input
-          type="text"
-          placeholder="Search city..."
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          className="search-input"
-        />
-        <button onClick={handleSearch} className="search-button-submit">
-          Search
-        </button>
-      </div>
+
     </div>
   );
 }
