5390ddf0484f151b846e74275388063ad1f0e7be
Todor Mitevski <74872391+todormitevski@users.noreply.github.com>
2025-10-13 19:44:43 +0200

    feature: add routing (#1)

--- duva-li/src/main.tsx
+++ duva-li/src/main.tsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from  './App'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import HeatMap from "./pages/HeatMap";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/heatmap" element={<HeatMap />} />
+    </Routes>
+  </BrowserRouter>
+);
