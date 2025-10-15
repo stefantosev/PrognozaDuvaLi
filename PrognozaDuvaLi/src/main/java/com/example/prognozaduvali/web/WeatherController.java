23b0bfc99495bdcccefcb80c2bcc05d56905dcbd
Walkorion <stefantosev15@gmail.com>
2025-10-15 18:29:41 +0200

    Added new url for frontend on backend

--- PrognozaDuvaLi/src/main/java/com/example/prognozaduvali/web/WeatherController.java
+++ PrognozaDuvaLi/src/main/java/com/example/prognozaduvali/web/WeatherController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/weather")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "https://prognoza-duva-li-jpd4.vercel.app/")
 public class WeatherController {
 
     private final WeatherService weatherService;
